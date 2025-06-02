a60828ec2501b4489a9d3be7ac8ca423ca39554e
Gabriel Sales <gabrielsales07112018@gmail.com>
2025-06-02 08:45:53 -0300

    feat: Fechei as rotas de criar e deletar userLocation para somente ADMIN

--- src/modules/userLocation/userLocation.controller.ts
+++ src/modules/userLocation/userLocation.controller.ts
@@ -3,6 +3,7 @@ import { Body, Controller, Delete, Get, ParseIntPipe, Post, Query } from "@nestj
 import { PaginationDTO } from "src/common/dto/pagination.dto";
 import { UserLocationService } from "./userLocation.service";
 import { UserLocationRequestDTO } from "./dto/user-location-request.dto";
+import { Roles } from "src/decorators/roles.decorator";
 
 @Controller('user-location')
 export class UserLocationController {
@@ -13,11 +14,13 @@ export class UserLocationController {
         return await this.userLocationService.findAllRelationshipsWithLocations(pagination)
     }
 
+    @Roles('ADMIN')
     @Post()
     async insert(@Body() data: UserLocationRequestDTO) {
         return await this.userLocationService.insert(data)
     }
 
+    @Roles('ADMIN')
     @Delete()
     async delete(
         @Query('userId', ParseIntPipe) userId: number,
